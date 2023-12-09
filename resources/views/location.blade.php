47a1a034a6080bddf0fc784a7c58acf91345b9e3
KiranDhungana <kirandhungana570@gmail.com>
2023-12-09 20:54:13 +0545

    okay

--- resources/views/location.blade.php
+++ resources/views/location.blade.php
@@ -8,15 +8,15 @@
 <body>
   
 <div class="container">
-      <iframe
-        width="600"
-        height="450"
-        frameborder="0"
-        style="border:0"
-        src="https://www.openstreetmap.org/?mlat={{ $currentUserInfo->latitude }}&mlon={{ $currentUserInfo->longitude }}";
-        
-        allowfullscreen
-    ></iframe>
+    <iframe
+    width="600"
+    height="450"
+    frameborder="0"
+    style="border:0"
+    src="https://www.google.com/maps/embed/v1/place?q={{ $currentUserInfo->latitude }},{{ $currentUserInfo->longitude }}&key=AIzaSyDqRk8ooQjKQgueGWQ485IjEO9t0-Y0CSQ"
+    allowfullscreen
+></iframe>
+
     <h1>How to Get Current User Location with Laravel - ItSolutionStuff.com</h1>
     <div class="card">
         <div class="card-body">
@@ -36,3 +36,4 @@
 </div>
   
 </body>
+
