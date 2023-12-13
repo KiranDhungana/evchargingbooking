ea9acd52f3e32e44c3af4ab4e02cc1e8a9c7ddd1
KiranDhungana <kirandhungana570@gmail.com>
2023-12-13 22:38:10 +0545

    reuser comp

--- resources/views/profile.blade.php
+++ resources/views/profile.blade.php
@@ -4,8 +4,8 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Profile</title>
-    <!-- @vite('resources/css/app.css') -->
-    <script src="https://cdn.tailwindcss.com"></script>
+     @vite('resources/css/app.css')
+    {{-- <script src="https://cdn.tailwindcss.com"></script> --}}
 </head>
 <style>
   .profile-container{
